2b093ef1771f1a196b83723df59e63edacfac436
reshmir26197 <reshmir26197@gmail.com>
2017-06-23 12:37:14 +0530

    Create set1 sign.py

---
+++ set1 sign.py
@@ -0,0 +1,10 @@
+def sign():
+	n=input('Enter a number')
+	if n==0:
+		print 'given number is 0'
+	if n>0:
+		print 'given number is positive'
+	if n<0:
+		print 'given number is negative'
+	return
+sign()
